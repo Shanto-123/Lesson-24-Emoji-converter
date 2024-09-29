abc0a9a84d72e1467cdce3f928976c8879f3a790
shanto das <114013679+Shanto-123@users.noreply.github.com>
2024-09-30 01:28:45 +0600

    Add files via upload

---
+++ Lesson-24-emoji-converter.py
@@ -0,0 +1,14 @@
+message = input("Type your message : ")
+
+separate_words=message.split(' ')
+print(separate_words)
+emoji = {
+
+    ":)":"😒",
+    ":(":"😂"
+
+}
+output = ""
+for word in separate_words:
+    output += emoji.get(word,word)+ " "
+    print(output)
